c91e2dbd53bfca5d1220d3b911e4d4846bb6b381
Noelle <happyhourxd2@proton.me>
2025-03-31 14:13:28 -0400

    added todo, started constructor to determine whether main is server or client

--- main.java
+++ main.java
@@ -1,3 +1,20 @@
 public class main {
     
+    //todo
+    //determine if client or server then act on that
+    //gameplay elements
+
+    static boolean isServer = false;
+    static int port = 5000;
+    static String addr = "127.0.0.1";
+
+    public static void main(String[] args) {
+        if (args[0] == "s") {
+            isServer = true;
+            server server = new server(port);
+        }
+        else {
+            client client = new client(addr, port);
+        }
+    }
 }
