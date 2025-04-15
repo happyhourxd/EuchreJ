31c5b70679470f2c350017c3aa28781780027fc9
Noelle <happyhourxd2@proton.me>
2025-04-15 15:13:32 -0400

    determined whether server or client + infinate while loop

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
+import java.util.*;
 public class Main {
     
     //todo
-    //determine if client or server then act on that
+    //determine if client or server by using arguments
     //gameplay elements
 
     static boolean isServer = false;
@@ -9,12 +10,21 @@ public class Main {
     static String addr = "127.0.0.1";
 
     public static void main(String[] args) {
-        if (args[0] == "s") {
+        if (args.length > 0) {
+            isServer = true;
+        }
+        if (!isServer) {
+            Client client = new Client(addr, port);
+            client.join();
+            while (true) {
+
+            }
+        } else {
             isServer = true;
             Server server = new Server(port);
-        }
-        else {
-            Client client = new Client(addr, port);
+            while (isServer) {
+                
+            }
         }
     }
 }
