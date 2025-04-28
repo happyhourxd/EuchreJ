b127c2de6bdd974e6535b4c0b54888a49f603bf3
Noelle <happyhourxd2@proton.me>
2025-04-28 12:43:22 -0400

    made trick insdead of game, will reset each trick for ease of use

---
+++ Trick.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+
+public class Trick {
+
+    public ArrayList<Player> players = new ArrayList<>();
+    public Deck deck;
+    public int[] score = {0,0};
+    public Card trump = null;
+    public Player dealer;
+    public int turn = 0;
+    public ArrayList<Card> table;
+
+    public Trick(ArrayList<Player> players) {
+        this.players = players;
+        this.deck = new Deck();
+        this.table = new ArrayList<>();
+
+        for (Player p : players) {
+            p.clearHand();
+        }
+    }
+
+    public Trick(ArrayList<Player> players, Card trump, int[] score) {
+        
+    }
+
+    public Trick deal() {
+        if (this.deck.getSize() < 24) {
+            this.deck = new Deck();
+            this.deck.shuffle();
+        }
+        deck.shuffle();
+        while(deck.getSize() > 6) {
+            players.get(0).giveCard(deck.drawCard());
+            players.get(1).giveCard(deck.drawCard());
+            players.get(2).giveCard(deck.drawCard());
+            players.get(2).giveCard(deck.drawCard());
+        }
+        return this;
+    }
+
+    public Card getTrump() {
+        return this.trump;
+    }
+
+    public void setTrump(Card trump) {
+        this.trump = trump;
+    }
+
+    public void normalTrump(Card card) {
+        this.trump = deck.getTop();
+        for (Player p : players) {
+            if (p.id == dealer.id) {
+                p.play(card);
+                p.giveCard(this.trump);
+            }
+        }
+    }
+
+    public Trick play(Card card) {
+        for (Player p : players) {
+            if (p.cards.contains(card)) {
+                p.play(card);
+            }
+        }
+        table.add(card);
+        return this;
+    }
+
+    public int score() {
+        return 0;
+    }
+
+    public void setDealer(int id) {
+        for (Player p : players) {
+            if (p.id == id)
+                dealer = p;
+        }
+    }
+
+    public Player getPlayer(int id) {
+        for (Player p : players) 
+            if (p.id == id)
+                return p;
+        return null;
+    }
+
+    public Player getDealer() {
+        return dealer;
+    }
+}
