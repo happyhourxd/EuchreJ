59173f6181c5cfb433d2ef75a5fd28a306789eb8
Noelle <happyhourxd2@proton.me>
2025-05-01 00:27:12 -0400

    made card more serializable and basic

--- Card.java
+++ Card.java
@@ -2,16 +2,19 @@ import java.io.Serializable;
 
 public class Card implements Serializable {
     
-    private static final long serialVersionUID = 00;
-    int number; // the number of the card 9 = 9, 10 = 10, J = 11, Q = 12, K = 13, A = 14
-    String suit; // hearts, spaces, clubs, diamands
-    int value;
+    private static final long serialVersionUID = 03;
+    final int number; // the number of the card 9 = 9, 10 = 10, J = 11, Q = 12, K = 13, A = 14
+    final String suit; // hearts, spaces, clubs, diamands
 
 
-    public Card(int number, String suit){ //just a card constructor
+    public Card(int number, String suit) { //just a card constructor
         this.number = number;
         this.suit = suit;
-        this.value = number;
+    }
+
+    public Card(Card card) {
+        this.number = card.number;
+        this.suit = card.suit;
     }
 
     public String toString() {
@@ -21,19 +24,8 @@ public class Card implements Serializable {
     public int getNumber() {
         return number;
     }
+
     public String getSuit() {
         return suit;
-    }
-    public int getValue() {
-        return value;
-    }
-    public void setNumber(int number) {
-        this.number = number;
-    }
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
-    public void setValue(int value) {
-        this.value = value;
     }
 }
