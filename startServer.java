7ce2c90d5501e00932a631778183b184b79e0987
ValkyrieAfterimage <reflectivelegacy@gmail.com>
2025-05-05 13:05:08 -0400

    added comment for usability

--- startServer.java
+++ startServer.java
@@ -1,10 +1,11 @@
+// run this file to start the server (required)
 public class startServer {
 
-    static int port = 5000;
+    static int port = 5000; // change to change port
 
     public static void main(String[] args) {
         try {
-            Server server = new Server(5000);
+            Server server = new Server(port);
         } catch (Exception e) {
             System.out.println(e);
         }
